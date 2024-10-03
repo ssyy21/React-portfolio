5f98689b3dae2a9c96e80ebcb2188ec825266a70
Sneha Yadav <you@example.com>
2024-10-03 21:28:39 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,78 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+
+
+export const HERO_CONTENT = `Hi, I’m Sneha Yadav, currently a second-year student pursuing a B.Tech in Computer Science and Artificial Intelligence at Indira Gandhi Delhi Technical University for Women (IGDTUW). I am deeply passionate about exploring the intersections of technology and innovation, and I thrive in dynamic environments where continuous learning is encouraged. My journey so far has involved working on various projects that span web development, machine learning giving me a comprehensive understanding of how these technologies are shaping the future. Alongside academics, I actively participate in leadership roles, such as Joint Executive of Team Adira at IGDTUW and Head Coordinator of the IEEE IGDTUW Web Admin team, to contribute to impactful initiatives.`;
+
+export const ABOUT_TEXT = `I am a tech enthusiast who loves exploring the ever-evolving world of artificial intelligence, machine learning, and data structures. Currently in my second year at IGDTUW, I am actively engaged in several technical communities and mentorship programs. I have had the opportunity to serve as a mentee in the DSA circle of the Google Developer Student Club and the ML circle of Lean In. In addition to my academic pursuits, I enjoy taking on leadership roles in various organizations, helping teams succeed while growing my own skills. I regularly participate in hackathons and collaborative projects that push me to develop innovative solutions. My aim is to continue honing my technical abilities while exploring new technologies that can make a significant impact.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Head Coordinator",
+    company: "Web Admin Team, IEEE IGDTUW",
+    description: `As the Head Coordinator, I manage the website's development and ensure smooth operations for IEEE IGDTUW's online platforms. I lead a team focused on maintaining the site, optimizing performance, and enhancing user experience.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2024 - present",
+    role: "Joint Executive ",
+    company: "Team Adira, Centre for Sustainable Development, IGDTUW",
+    description: `In this role, I collaborate on sustainability-driven projects, initiatives to raise awareness and promote sustainable practices. I also coordinate events and campaigns across the university.`,
+    technologies: ["Leadership", "Teamwork", "Event management"],
+  },
+  {
+    year: "2023 - 2023",
+    role: "Member-Technical team",
+    company: "AWS Cloud Club IGDTUW",
+    description: `Developed and maintained AWS Cloud clubs official website using JavaScript, HTML, CSS. Collaborated with team members and made team projects.`,
+    technologies: ["Python", "HTML", "CSS", "Javascript"],
+  },
+  {
+    year: "2024 - 2024",
+    role: "Python and Machine Learning Intern",
+    company: "COE-AI IGDTUW",
+    description: `I completed an 8-week internship focused on Python programming and Machine Learning, where I built and optimized ML models for data-driven projects. This experience helped me gain a deeper understanding of model development, data preprocessing, and algorithm selection.`,
+    technologies: ["Python", "Pandas", "NumPy,", "SScikit-learn"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "NatureNexus",
+    image: project1,
+    description:
+      "An e-commerce website with features like product listing, shopping cart, and impact calculator.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "ML Model for prediction of cervical cancer",
+    image: project4,
+    description:
+      "A ML model for Prediction of Cervical Cancer, where several preprocessing methods, along with various classification models are used and a highly efficient model was developed.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  // {
+  //   title: "Children Education Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
+];
+
+export const CONTACT = {
+  address: "Air Force Camp, New Delhi ",
+  phoneNo: "+91 6352292XXX ",
+  email: "sy0381718@gmail.com",
+};
