29d3cb881d8fda9662925b89716e90fcdf0e99e5
Sneha Yadav <143681944+ssyy21@users.noreply.github.com>
2024-10-04 06:20:25 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/Profile.jpg";
+import profilePic from "../assets/profile.jpg";
 import { delay, motion } from "framer-motion";
 
 const container =(delay) => ({
